c6041d264503cf8bfeb291bd701ec5aac15f2b29
rishijain <rishi@joshsoftware.com>
2012-12-06 18:08:26 +0530

    added project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatatablesTalk::Application.config.secret_token = '3ce8d7fb2822f2d68f93bc00886f31dfb3829aa87541b5dc10441bc946a623bb8ef450d0a18e4a6bd31f8ea3fe6bdb5acc007a6d6bcacac515a4383ad49b0cf8'
