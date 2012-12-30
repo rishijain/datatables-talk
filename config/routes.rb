510b70b9c0831c15d73196475f9c7bba94c7a069
rishijain <rishi@joshsoftware.com>
2012-12-30 10:02:46 +0530

    included impress.js

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ DatatablesTalk::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
   resources :users
+  match '/presentation' => 'home#presentation', :as => :presentation_path
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
